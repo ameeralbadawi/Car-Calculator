74c9cf2c2f1bde12545508a9cf19d2c18ba7a524
“ameeralbadawi” <ameeralbadawi@gmail.com>
2025-08-20 14:37:16 -0500

    added react-router-dom to vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@clerk/clerk-react']
+      external: [
+        '@clerk/clerk-react',
+        'react-router-dom'
+      ]
     }
   }
 })
