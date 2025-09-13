910f9a4972046962b6fabc342e0a4a95b2e938c1
“ameeralbadawi” <ameeralbadawi@gmail.com>
2025-09-13 14:28:21 -0500

    comitting Home.jsx along with vite.config.js cleanup

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   base: './',
   plugins: [react()],
-  // REMOVE the build.rollupOptions.external section entirely
   build: {
     commonjsOptions: {
       transformMixedEsModules: true
     }
   }
 })
+
