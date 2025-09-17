fb8becda33368ff9e3ce836b9eb69c304ead8104
“ameeralbadawi” <ameeralbadawi@gmail.com>
2025-09-17 14:55:34 -0500

    added "mobile app coming soon" image to Home

--- vite-project/src/Home.jsx
+++ vite-project/src/Home.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
-import { Typography, Box } from '@mui/material'
+import { Typography, Box, Paper } from '@mui/material'
+import RenderedImage from './assets/RenderedImage.jpeg';
 
 function Home() {
     return (
-        <Box sx={{ textAlign: 'center'}}>
-            <Typography variant="h6" gutterBottom>
-            Experiencing issues? Contact our support team at support@carvintory.com and we’ll help you out.
-            </Typography>
-        </Box>
+        <>
+            <Box sx={{ textAlign: 'center' }}>
+                <Typography variant="h6" gutterBottom>
+                    Experiencing issues? Contact our support team at support@carvintory.com and we’ll help you out.
+                </Typography>
+            </Box>
+            <Box display="flex" justifyContent="center" mt={4}>
+                <Paper
+                    elevation={3}
+                    sx={{
+                        p: 2,
+                        borderRadius: 3,
+                        width: "80vw",       // 80% of the viewport width
+                        maxWidth: "1200px",  // prevent it from being huge on big screens
+                    }}
+                >
+                    <img
+                        src={RenderedImage}
+                        alt="Mobile App Coming Soon"
+                        style={{
+                            width: "100%",   // Image fills the Paper
+                            height: "auto",  // Keep aspect ratio
+                            display: "block",
+                            borderRadius: 10,
+                        }}
+                    />
+                </Paper>
+            </Box>
+
+
+        </>
     )
 }
 
